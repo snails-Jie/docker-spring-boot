8c9a5db99462e8fd5686a4c4f70b54ab819a415b
zhangjie <731255090@qq.com>
2023-02-19 00:01:39 +0800

    test

--- src/main/java/com/docker/demo/controller/DemoController.java
+++ src/main/java/com/docker/demo/controller/DemoController.java
@@ -10,7 +10,7 @@ public class DemoController {
 
     @RequestMapping("/home")
     public String home() {
-        log.info("Hello Docker World");
+        log.info("Hello Docker World test");
         return "Hello Docker World";
     }
 }
